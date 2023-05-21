3acbcc6da2a17e8599fcec26bebd8acabc242ee7
Sanskriti Garg <77229345+Sankriti09@users.noreply.github.com>
2023-05-21 20:50:33 +0530

    Update Schema.sql

--- DAY 5/Schema.sql
+++ DAY 5/Schema.sql
@@ -1,1 +1,31 @@
 
+-- Table Script
+
+CREATE TABLE Online_Activity
+(
+    DATE DATE ,
+    Activity_Code varchar(20),
+    Value int
+);
+
+INSERT INTO DATA1 VALUES
+		    ('01-10-2021', 'Impressions', 23),
+	      ('02-10-2021', 'Impressions', 2000),
+        ('03-10-2021', 'Impressions', 3000),
+        ('04-10-2021', 'Impressions', 2390),
+        ('05-10-2021', 'Impressions', 2322),
+        ('06-10-2021', 'Impressions', 232),
+        ('07-10-2021', 'Impressions', 111),
+        ('08-10-2021', 'Impressions', 4343),
+        ('09-10-2021', 'Impressions', 5554),
+        ('10-10-2021', 'Impressions', 65767),
+        ('01-10-2021', 'Clicks', 1),
+        ('02-10-2021', 'Clicks', 20),
+        ('03-10-2021', 'Clicks', 40),
+        ('04-10-2021', 'Clicks', 27),
+		    ('05-10-2021', 'Clicks', 30),
+		    ('06-10-2021', 'Clicks', 44),
+		    ('07-10-2021', 'Clicks', 33),
+		    ('08-10-2021', 'Clicks', 12),
+		    ('09-10-2021', 'Clicks', 56),
+		    ('10-10-2021', 'Clicks', 10);
