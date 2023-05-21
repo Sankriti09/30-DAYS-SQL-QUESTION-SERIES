d45d35188cb279aebf4e0a632c0d3b2d72dc1d49
Sanskriti Garg <77229345+Sankriti09@users.noreply.github.com>
2023-05-21 19:32:19 +0530

    Update Query.sql

--- DAY 4/Query.sql
+++ DAY 4/Query.sql
@@ -1,1 +1,15 @@
+--- DAY 4
 
+
+-- ❓Display all the employees who are getting more than the average salaries of all the employees. 
+SELECT ename, job, sal
+FROM(SELECT ename, job,
+				 sal,
+				 FLOOR(AVG(sal) OVER()) as avg_sal
+		 FROM employee_data
+     )a
+WHERE sal>avg_sal
+ORDER BY 1;
+
+
+-- author : @sanskriti_garg
