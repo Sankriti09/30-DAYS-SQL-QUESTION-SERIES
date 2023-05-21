83e28936c1ba455906beff14baca4d691059b030
Sanskriti Garg <77229345+Sankriti09@users.noreply.github.com>
2023-05-21 23:42:47 +0530

    Update Query.sql

--- DAY 7/Query.sql
+++ DAY 7/Query.sql
@@ -1,1 +1,16 @@
 
+--day 7
+
+--❓ Display all the employees who are getting some commission in thesales department where the employees have joined only on weekdays.
+SELECT ename, comm, dname, day
+FROM(SELECT ename, comm, d.dname,
+            WEEKDAY(HireDate) AS Day
+     FROM employee_data e
+     INNER JOIN department_data d ON e.deptno = d.deptno
+     WHERE d.dname IN('Sales') AND e.comm>0)a
+WHERE a.Day BETWEEN 0 AND 4;
+
+
+
+
+--AUTHOR : @Sanskriti_garg
