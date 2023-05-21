b094f26eb7ac5d519120c9b2af79f83a19f495a1
Sanskriti Garg <77229345+Sankriti09@users.noreply.github.com>
2023-05-21 23:24:30 +0530

    Update Query.sql

--- DAY 6/Query.sql
+++ DAY 6/Query.sql
@@ -1,1 +1,18 @@
 
+-- DAY 6
+
+-- ❓ Display the second maximum salary in each department.
+SELECT deptno, dname,sal AS Second_maximum_salary
+FROM(SELECT d.deptno, d.dname,e.empno, ename,job,sal,
+            DENSE_RANK() OVER(PARTITION BY e.deptno ORDER BY sal desc)AS rnk
+     FROM employee_data AS e
+     INNER JOIN department_data AS d
+         ON e.deptno = d.deptno
+     )a
+WHERE a.rnk=2
+GROUP BY 1;
+
+
+
+
+-- author : @Sanskriti_Garg
