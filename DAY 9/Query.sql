cad1d72d880cbbe0386949ba8be93168126e9859
Sanskriti Garg <77229345+Sankriti09@users.noreply.github.com>
2023-05-22 00:40:49 +0530

    Update Query.sql

--- DAY 9/Query.sql
+++ DAY 9/Query.sql
@@ -1,1 +1,21 @@
 
+-- DAY 09
+
+-- ❓
+SELECT customer_id, 
+       GROUP_CONACT(product_name) as MOST_POPULAR_PRODUCT
+FROM(SELECT s.customer_id
+          , m.product_name
+          , COUNT(s.product_id) as highest_ordered_product
+          , DENSE_RANK() OVER(PARTITION BY customer_id ORDER BY COUNT(s.product_id) DESC) as rnk
+     FROM sales s
+     INNER JOIN menu m 
+        ON s.product_id = m.product_id
+     GROUP BY 1,2
+     )a
+WHERE rnk=1
+GROUP BY 1;
+
+
+
+-- author : @Sanskriti_Garg
