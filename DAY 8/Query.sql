21d331bc0b0eb0ff05b4af05cb3e96d6d8d3dc6b
Sanskriti Garg <77229345+Sankriti09@users.noreply.github.com>
2023-06-25 15:11:42 +0530

    Update Query.sql

--- DAY 8/Query.sql
+++ DAY 8/Query.sql
@@ -1,1 +1,16 @@
 
+--day 8
+
+--❓ Display only the non-matching records from the department table 
+--📍Assumption - (matching records shouldn’t be selected)
+SELECT d.deptno
+    , e.job
+    , d.dname
+    , d.loc
+FROM department_data AS d
+LEFT JOIN employee_data AS e
+    ON e.deptno = d.deptno
+WHERE e.deptno IS NULL;
+
+
+--AUTHOR : @Sanskriti_garg
