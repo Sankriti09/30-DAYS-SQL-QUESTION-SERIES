11d62b91bde1f96d5519e925bf793fec02797105
Sanskriti Garg <77229345+Sankriti09@users.noreply.github.com>
2023-06-25 23:25:53 +0530

    Update Query.sql

--- DAY 14/Query.sql
+++ DAY 14/Query.sql
@@ -1,1 +1,21 @@
+WITH cte AS
+(    
+     SELECT ename
+          , sal
+          , deptno
+          , ROW_NUMBER() OVER(PARTITION BY deptno ORDER BY sal) as r_n
+      FROM employee_data
+),
 
+cte1 AS
+(
+      SELECT *,
+            LAST_VALUE(r_n) OVER(PARTITION BY deptno ORDER BY r_n RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)as l_value
+      FROM cte
+)
+
+SELECT deptno
+     , Round(AVG(sal),0) as median_salary
+FROM cte1
+WHERE r_n BETWEE l_value/2 AND ((l_value/2)+1)   
+GROUP BY deptno;
