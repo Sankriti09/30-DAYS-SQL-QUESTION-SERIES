851df86e308cf55e465420176bcacd6a9f21598e
Sanskriti Garg <77229345+Sankriti09@users.noreply.github.com>
2023-07-09 14:02:53 +0530

    Update Query.sql

--- DAY 18/Query.sql
+++ DAY 18/Query.sql
@@ -1,1 +1,12 @@
 
+-- DAY 18 | Question 18
+
+-- ❓Display the employees who are earning the same salary in the table.
+SELECT e.empno
+     , e.ename
+     , e.job
+     , e.sal
+FROM EMPLOYEE_DATA e
+INNER JOIN EMPLOYEE_DATA d ON e.sal = d.sal 
+WHERE e.empno<>d.empno
+ORDER BY 1;
