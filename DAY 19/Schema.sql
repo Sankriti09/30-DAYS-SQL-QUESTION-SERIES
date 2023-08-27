d37c0952264b31dc0e170df2387d0534af69cce7
Sanskriti Garg <77229345+Sankriti09@users.noreply.github.com>
2023-08-27 15:34:26 +0530

    Update Schema.sql

--- DAY 19/Schema.sql
+++ DAY 19/Schema.sql
@@ -1,1 +1,19 @@
+--TABLE SCRIPT -
 
+CREATE TABLE user_transaction(
+ user_id varchar(25),
+ visit int,
+ make_order char(10));
+ 
+INSERT INTO user_transaction VALUES
+ ('4a495637', 2, 'true'),
+ ('0288950d', 3, 'false'),
+ ('434d93d7', 4, 'true'),
+ ('0cf653b3', 5, 'true'),
+ ('e4046cd7796c',0, 'false'),
+ ('4e5cc7cc3743',1, 'false'),
+ ('10bc8ea99240',6, 'true'),
+ ('545a8c79deab',3, 'false'),
+ ('d9de62380374',3, 'true'),
+ ('768ec99c', 7, 'true'); 
+ ('e4c9a45e1c2c',0, 'false');
