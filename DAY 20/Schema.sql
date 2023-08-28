111a578e9ec03f2f4fc78a3268a783063e236a80
Sanskriti Garg <77229345+Sankriti09@users.noreply.github.com>
2023-08-29 00:14:31 +0530

    Update Schema.sql

--- DAY 20/Schema.sql
+++ DAY 20/Schema.sql
@@ -1,1 +1,16 @@
+--TABLE SCRIPT -
 
+CREATE TABLE USERS(
+ user_id int,
+ action varchar(20),
+ action_date date)
+ ;
+INSERT INTO USERS VALUES
+ (1, 'Start', '2020-02-12'),
+ (1, 'Cancel', '2020-02-13'),
+ (2, 'Start', '2020-02-11'),
+ (2, 'Publish', '2020-02-14'),
+ (3, 'Start', '2020-02-15'),
+ (3, 'Cancel', '2020-02-15'),
+ (4, 'Start', '2020-02-18'),
+ (1, 'Publish', '2020-02-19');
