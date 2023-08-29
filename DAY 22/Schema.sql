baa499d0b2f46cbe329b2fde953800c6266c53db
Sanskriti Garg <77229345+Sankriti09@users.noreply.github.com>
2023-08-30 01:30:16 +0530

    Update Schema.sql

--- DAY 22/Schema.sql
+++ DAY 22/Schema.sql
@@ -1,1 +1,23 @@
+-- Table script - 
 
+CREATE TABLE ACCOUNTS(
+ AccountNo int,
+ Name varchar(40),
+ Balance int);
+
+INSERT INTO ACCOUNTS VALUES
+ (101, 'Venkat Ratnam', 1000),
+ (102, 'Abhinav', 4000);
+ 
+CREATE TABLE TRANSACTIONS(
+ AccountNo int,
+ TransactionID int,
+ TransactionDate date,
+ TransactionType char(2),
+ TransactionAmount int);
+
+INSERT INTO TRANSACTIONS VALUES
+ (101, 1, '2011-04-01','D',300), 
+ (101, 2, '2011-04-02','W',500),
+ (101, 3, '2011-04-03','W',200),
+ (101, 4, '2011-04-04','D',44); 
