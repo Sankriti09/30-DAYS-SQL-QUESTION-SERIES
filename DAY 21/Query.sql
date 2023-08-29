cb442419536e047c586ee7037008cd8d5a8a7910
Sanskriti Garg <77229345+Sankriti09@users.noreply.github.com>
2023-08-30 00:36:52 +0530

    Update Query.sql

--- DAY 21/Query.sql
+++ DAY 21/Query.sql
@@ -1,1 +1,18 @@
 
+-- Day 21 | Question 21
+
+-- ❓ Write an SQL query to find all dates and IDs with higher temperatures compared to their previous dates (yesterday).
+WITH temperature AS
+(
+  SELECT id
+       , recordDate
+       , temperature
+       , COALESCE(LAG(temperature) OVER(ORDER BY recordDate),temperature) AS previous_temperature
+  FROM weather
+)
+
+SELECT id, recordDate
+FROM temperature
+WHERE temperature > previous_temperature;
+
+-- author : Sanskriti_Garg
