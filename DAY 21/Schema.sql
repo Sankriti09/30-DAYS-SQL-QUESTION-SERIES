a995352a4c90ff3c8de72606c42a1658c3c77bba
Sanskriti Garg <77229345+Sankriti09@users.noreply.github.com>
2023-08-30 00:31:16 +0530

    Update Schema.sql

--- DAY 21/Schema.sql
+++ DAY 21/Schema.sql
@@ -1,1 +1,12 @@
+--Table script -
 
+CREATE TABLE WEATHER(
+ id int,
+ recordDate Date,
+ temperature int
+);
+INSERT INTO WEATHER VALUES
+ (1, '2015-01-01',10),
+ (2, '2015-01-02',25),
+ (3, '2015-01-03',20),
+ (4, '2015-01-04',30);
