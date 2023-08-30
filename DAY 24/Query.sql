ad710abe53dd2657ae8588e895ceaba7955f6cca
Sanskriti Garg <77229345+Sankriti09@users.noreply.github.com>
2023-08-30 13:27:09 +0530

    Update Query.sql

--- DAY 24/Query.sql
+++ DAY 24/Query.sql
@@ -1,1 +1,11 @@
 
+-- DAY 24 | QUESTION 24|
+
+-- ❓  Write a query to fetch the employee names and their salary even if the salary record is not present for the employee.
+SELECT e.FullName, s.Salary
+FROM Employee_details e
+LEFT JOIN Employee_Salary s
+    ON e.EmpId = s.EmployeeId
+ORDER BY 1;
+
+-- author :Sanskriti_garg
